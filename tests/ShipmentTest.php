f08255b07bcfc1d6a1f53c82b40c2a5555f1aed4
kabusaid <saidkh@gmail.com>
2016-08-29 22:17:52 +0300

    Update ShipmentTest.php

--- tests/ShipmentTest.php
+++ tests/ShipmentTest.php
@@ -17,16 +17,6 @@ class ShipmentTest extends TestCase
     }
 
     /** @test */
-    public function shipment_price_cannot_be_less_than_10(){
-        $product = new Product();
-        $product->setWeight(4);
-
-        $shippingCalculator = new ShipmentCalculator();
-
-        $this->assertEquals(10, $shippingCalculator->calculateProduct($product));
-    }
-
-    /** @test */
     public function calculate_the_total_shipment_price_for_the_whole_cart(){
         $product1 = new Product();
         $product1->setWeight(4);
@@ -41,5 +31,16 @@ class ShipmentTest extends TestCase
 
         $this->assertEquals(22, $shippingCalculator->calculateCart($cart));
     }
+    
+    /** @test */
+    public function shipment_price_cannot_be_less_than_10(){
+        $product = new Product();
+        $product->setWeight(4);
+
+        $shippingCalculator = new ShipmentCalculator();
+
+        $this->assertEquals(10, $shippingCalculator->calculateProduct($product));
+    }
+
 }
 ?>
