78d7f67c46ecc2c9c65401a8293ed58cc7a59e33
kabusaid <saidkh@gmail.com>
2016-08-30 22:52:40 +0300

    Update Cart.php

--- src/Cart.php
+++ src/Cart.php
@@ -1,19 +1,27 @@
 <?php
 
-class Cart {
+class Cart implements ShipInterface {
 
-    public  $products = [];
+    private $products = [];
+
+    /**
+     * Implement the weight function of ShipInterface
+     */
+
+    public function weight(){
+        $totalWeight = 0;
+        foreach ($this->products as $product) {
+            $totalWeight += $product->getWeight();
+        }
+        return $totalWeight;
+    }
+
+    /**
+     * Add Product to Cart
+     */
 
     public function addToCart($product)   {
         $this->products[] = $product;
-    }
-
-    public function getTotalWeight()   {
-        $total = 0;
-        foreach ($this->products as $product) {
-            $total += $product->getWeight();
-        }
-        return $total;
     }
 
 }
